9e877d2153a7d63828f82c4314a430cce211b168
Alexey Reshetnyak <alexreshetnyakmail@gmail.com>
2019-06-03 18:40:14 +0300

    began work with if statment

---
+++ 13_if_statments.rb
@@ -0,0 +1,25 @@
+
+if 5 < 7
+  p 'True'
+end
+
+word = 'zebra'
+if word.include?('eb')
+  p 'Yes'
+end
+
+if ''
+  p 'Empty string is true'
+end
+
+if 0
+  p '0 is true'
+end
+
+if nil
+  p 'nil is false'
+end
+
+if false
+  p 'false is false'
+end
