527555df246f733cfbdb395af63af3ea78eb0daa
Alexey Reshetnyak <alexreshetnyakmail@gmail.com>
2019-06-04 17:26:04 +0300

    lerned new details about if statements

--- 13_if_statments.rb
+++ 13_if_statments.rb
@@ -23,3 +23,64 @@ end
 if false
   p 'false is false'
 end
+
+p '-----------------'
+color = 'red'
+
+if color == 'green'
+  puts 'Color is green'
+elsif color == 'red'
+  puts 'Color red'
+else
+  puts 'Color not green'
+end
+
+p '------------------'
+
+def odd_or_even(number)
+  if number.odd?
+    'Thats number is odd'
+  else
+    'Thats number is even'
+  end
+end
+
+p odd_or_even(2)
+
+p '------------------'
+age = 25
+
+if age > 18 && age < 60
+  p 'You can go'
+end
+
+if age == 21 || age == 25
+  p 'Your age 21 or 25'
+end
+
+p '------------------'
+def authenticate_agent(rank, name, credentials)
+  if (rank == '007' && name == 'James Bond') || credentials == 'Secret Agent'
+    p 'Hello secret agent'
+  else
+    p 'Access denied ' + name
+  end
+end
+
+authenticate_agent('007', 'James Bond', 'Spy')
+
+p '------------------'
+def meal_plan(time_of_week, time_of_day)
+  if time_of_week == 'weekday'
+    if time_of_day == 'breakfarst'
+      'cereal'
+    end
+
+  elsif time_of_week == 'weekend'
+    if time_of_day == 'lunch'
+      'BBQ Chicken pizza'
+    end
+  end
+end
+
+p meal_plan('weekend', 'lunch')
