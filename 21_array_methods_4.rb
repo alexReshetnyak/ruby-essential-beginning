ef592de48c97c6ac777b3bfb93851c49e52da392
A Reshetnyak <alexreshetnyakmail@gmail.com>
2019-06-07 21:53:37 +0300

    learned uniq method

--- 21_array_methods_4.rb
+++ 21_array_methods_4.rb
@@ -1,9 +1,13 @@
 p '--------find---------'
-words = ['word1', 'wqwe', 'newword', 'else']
+words = ['word1', 'wqwe', 'newword', 'else', 'else']
 
 # * find first result
-p words.find{ |word| word.length > 3 }
+p words.find { |word| word.length > 3 }
 
 p '--------detect---------'
 # * worked like find
-p words.find{ |word| word.length > 3 }
+p words.find { |word| word.length > 3 }
+
+p '--------uniq method---------'
+# * made all values uniq
+p words.uniq
