1739f2980479875ba69305df08a01d4032fcad48
Alexey Reshetnyak <alexreshetnyakmail@gmail.com>
2019-06-10 11:03:39 +0300

    learned new details about procs

--- 28_yield.rb
+++ 28_yield.rb
@@ -17,7 +17,6 @@ end
 who_am_i { "Handsome" } # * call method using curly braces
 
 p '-----------------'
-
 def multiple_pass
   p 'Inside the block'
   yield
@@ -27,3 +26,57 @@ end
 
 result = multiple_pass { p "Now I'm inside the block" } # * call method using curly braces
 p result # * => "Now I'm inside the block"
+
+p '--------block_given? method---------'
+def pass_control_on_conditional
+  p 'Inside the method'
+
+  yield if block_given? # * pass control to block only if block exist
+
+  p 'Back inside the method'
+end
+
+pass_control_on_conditional do
+  p 'Hello from inside'
+end
+
+p '--------yielding with arguments---------'
+def speak_the_truth
+  yield 'Alex' if block_given?
+end
+
+speak_the_truth { |name| p "My name is #{name}" }
+
+def speak_the_truth2(name)
+  yield name if block_given?
+end
+
+speak_the_truth2('Alex') do |name, age|
+  p age
+  p "My name is #{name}"
+end
+
+# * return different values depend if we have block or not
+def speak_the_truth3(name)
+  return yield name if block_given?
+  name
+end
+
+p speak_the_truth3('Alex')
+p speak_the_truth3('Alex') { |name| "#{name} Reshetnyak" }
+
+p '--------custom each method---------'
+def custom_each(array)
+  i = 0
+  while  i < array.length
+    yield array[i]
+    i += 1
+  end
+end
+
+names = ['Alex', 'Boris', 'Melisa']
+numbers = [10, 20, 30]
+
+custom_each(names) do |name|
+  p "#{name} has length: #{name.length}"
+end
--- 29_procs.rb
+++ 29_procs.rb
@@ -1,4 +1,5 @@
 # * Procs is more effitient way to do something in a loop
+# * You can use Proc everywhere instead block
 
 cubes = Proc.new { |number| number ** 3 }
 squares = Proc.new { |number| number ** 2 }
@@ -24,11 +25,13 @@ p c_cubes
 p '------Second example-----'
 currencies = [10, 20, 30, 40, 50]
 
+
 to_euros = Proc.new { |currency| currency * 0.95 }
 to_pesos = Proc.new { |currency| currency * 20.70 }
 
 p currencies.map(&to_euros)
 p currencies.map(&to_pesos)
+
 
 p '------3 example-----'
 ages = [10, 60, 92, 30, 43, 30]
@@ -42,3 +45,40 @@ def filter_age(age)
   age > 50
 end
 p ages.select { |age| filter_age(age) }
+
+
+p '--------proc + yield---------'
+def greeter
+  p 'I am inside greeter'
+  yield
+end
+
+phrase = Proc.new do
+  p 'Inside the proc'
+end
+
+greeter(&phrase)
+
+
+p '-----pass method as Proc----'
+p ['1', '2', '3'].map { |number| number.to_i }
+p ['1', '2', '3'].map(&:to_i) # * use ruby to_i method as Proc
+p [1, 2, 3, 4, 5].select(&:even?)
+
+p '-----method with Proc parameters----'
+def talk_about(name, &myproc)
+  p "Let me tell you about #{name}"
+
+  myproc.call(name)
+end
+
+good_things = Proc.new do |name|
+  p "#{name} is a genius!"
+end
+
+bad_things = Proc.new do |name|
+  p "#{name} is a dolt!"
+end
+
+talk_about('Alex', &good_things)
+talk_about('Brock', &bad_things)
