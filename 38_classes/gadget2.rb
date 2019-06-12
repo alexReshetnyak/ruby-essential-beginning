c6fa160bf6e3abd20e191a95abed82f6b70a3aed
Alexey Reshetnyak <alexreshetnyakmail@gmail.com>
2019-06-12 18:31:44 +0300

    added custom setter method

--- 38_classes/gadget2.rb
+++ 38_classes/gadget2.rb
@@ -1,9 +1,9 @@
 p '----Create a class----'
 
 class Gadget
-  attr_accessor :password  # * Getter and setter in the same time
+  # attr_writer :password  # * Getter and setter in the same time
   attr_reader :production_number # * getter
-  attr_writer :username # * setter
+  attr_accessor :username, :password # * setter
 
   def initialize(username = 'default name', password = 'default password') # * worked like constructor method in JS, it calls every time when we creat new object (private)
     @username = username # * Instance variable (private)
@@ -13,6 +13,10 @@ class Gadget
 
   def to_s
     "Overwrited .to_s method, username #{@username}"
+  end
+
+  def password=(new_password) # * custom setter method, overwrite attr_accessor
+    @password = new_password if validate_password(new_password)
   end
 
   private # * all methods bellow are private
@@ -25,7 +29,14 @@ class Gadget
     5.times { middle_digits << alphabet.sample }
     "#{start_digits}-#{middle_digits}-#{end_digits}"
   end
+
+  def validate_password(new_password)
+    new_password.is_a?(String) && new_password.length >= 6 && new_password =~ /\d/
+  end
 end
 
-phone = Gadget.new('user', 'password')
+phone = Gadget.new('user', 'password123')
 p phone.production_number
+p phone.password
+phone.password = '1234567'
+p phone.password
