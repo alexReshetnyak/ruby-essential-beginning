c22eb9b39a3b62907159e63742d9a99d282764f8
AReshetnyak <areshetnyak@qualium-systems.com.ua>
2019-07-01 11:27:14 +0300

    experimented with private methods

--- 38_classes/car.rb
+++ 38_classes/car.rb
@@ -1,16 +1,23 @@
 # * protected methods
 
 class Car
-  def initialize(age, miles)
+
+  attr_reader :model
+
+  def initialize(age, miles, model)
     base_value = 20000
     age_deduction = age * 1000
     miles_deduction = (miles / 10.to_f)
 
     @value = base_value - age_deduction - miles_deduction
+    @model = generate_model_name(model)
   end
 
   def compare_car_with(car)
     self.value > car.value ? 'Your car is better!' : 'Your car is worse'
+
+    p generate_model_name('McLaren')
+    self.model
   end
 
   protected # * All methods bellow are protected
@@ -18,8 +25,14 @@ class Car
   def value
     @value
   end
+
+  private
+
+  def generate_model_name(name)
+    name.length.to_s << ' ' + name
+  end
 end
 
-civic = Car.new(3, 30000)
-fiat = Car.new(1, 20000)
+civic = Car.new(3, 30000, 'civic')
+fiat = Car.new(1, 20000, 'fiat')
 p civic.compare_car_with(fiat)
